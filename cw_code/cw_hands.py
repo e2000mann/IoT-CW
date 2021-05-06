a662d5bdf4df5e6322405bd0aa4429ba17181292
up887818 <57417766+up887818@users.noreply.github.com>
2021-05-06 15:25:33 +0100

    update 6 may

--- cw_code/cw_hands.py
+++ cw_code/cw_hands.py
@@ -9,8 +9,6 @@ import math
 
 # global variables
 # origins[0] is left, origins[1] is right
-# frame is from (0, 0) bottom left to (1, 1) top right
-# global origin is (0.5, 0.5)
 origins = [(-1, -1), (-1, -1)]
 
 inner_annulus = ["TRIANGLE", "RIGHT", "CIRCLE", "DOWN",
@@ -27,23 +25,21 @@ def button_mode(landmarks, hand, palm):
     # check hand's radius (distance) from global origin
     radius = line_distance(0.5, palm[0], 0.5, palm[1])
     radius = abs(radius)
-    print(radius)
-    if radius < 0.25:
-        # neutral position
-        pass
-    else:
+    if radius >= 0.25:
+        # hand not in neutral position
         # button needs to be pressed
         # check how many degrees from y axis hand is
-        line_c = line_distance(0.5, palm[0], 0.5 + radius, palm[1])
+        line_c = line_distance(0.5, palm[0], 1, palm[1])
         line_c = abs(line_c)
+
         # lines a and b are just radius (radius)
-        cos_angle = (line_c ** 2) + (2 * radius ** 2) / (2 * radius ** 2)
+        cos_angle = (line_c ** 2) / (2 * radius ** 2)
         print(cos_angle)
         angle = math.degrees(math.acos(cos_angle))
         print(str(angle))
 
-        if radius >= 0.25 and radius < 0.375:
-            #inner annulus
+        if radius < 0.375:
+            # inner annulus
             button = inner_annulus[0]
         else:
             # outer annulus
@@ -55,55 +51,73 @@ def joystick_mode(landmarks, hand, centre):
     if origins[hand] == (-1, -1):
         origins[hand] = centre
 
-    # else determine radius
+    # else determine distance between centre and current position
     else:
-        difference_x = origins[hand][0] + centre[0]
-        difference_y = origins[hand][1] + centre[1]
-
-    pass
+        distance = line_distance(origins[hand][0], centre[0],
+                                 origins[hand][1], centre[1])
 
 
 def hand_details(landmarks):
+    frame_landmarks = get_frame_coords(landmarks)
 
-    open = True
-
-    # 0 = index, 1 = middle,  2 = ring, 3 = pinky
-    mcps = [landmarks.landmark[5], landmarks.landmark[9],
-            landmarks.landmark[13], landmarks.landmark[17]]
-    tips = [landmarks.landmark[8], landmarks.landmark[12],
-            landmarks.landmark[16], landmarks.landmark[20]]
-
-    for finger in range(4):
-        if mcps[finger].y < tips[finger].y:
-            open = False
-            break
-
-    # hand 1 = right, hand 0 = left
-    if mcps[0].x < mcps[1].x:
-        hand = 1
-    else:
-        hand = 0
-
-    centre = get_palm_centre(landmarks)
+    open = check_if_hand_open(frame_landmarks)
+    hand = check_left_right(frame_landmarks[:2])
+    centre = get_palm_centre(frame_landmarks[0], frame_landmarks[5],
+                             frame_landmarks[17])
 
     if open:
         print("{} hand is open".format(hand))
-        joystick_mode(landmarks, hand, centre)
+        joystick_mode(frame_landmarks, hand, centre)
     else:
         print("{} hand is closed".format(hand))
-        button_mode(landmarks, hand, centre)
+        button_mode(frame_landmarks, hand, centre)
 
 
-def get_palm_centre(landmarks):
+def get_frame_coords(landmarks):
+    frame_landmarks = []
+
+    for point in mp_hands.HandLandmark:
+        normalised = landmarks.landmark[point]
+        pixel_coord =\
+        mp_drawing._normalized_to_pixel_coordinates(normalised.x, normalised.y,
+                                                    image_width, image_height)
+
+        frame_coord = ((pixel_coord[0] / image_width),
+                       (pixel_coord[1] / image_height))
+
+        frame_landmarks.append(frame_coord)
+
+    return frame_landmarks
+
+
+def get_palm_centre(bottom, side_1, side_2):
     # 0 = bottom, 5 = edge 1, 17 = edge 2
-    palm_bottom = landmarks.landmark[0]
-    palm_side_1 = landmarks.landmark[5]
-    palm_side_2 = landmarks.landmark[17]
+    avg_x = (bottom[0] + side_1[0] + side_2[0])/3
+    avg_y = (bottom[1] + side_1[1] + side_2[1])/3
 
-    palm_avg_x = (palm_bottom.x + palm_side_1.x + palm_side_2.x)/3
-    palm_avg_y = (palm_bottom.y + palm_side_1.y + palm_side_2.y)/3
+    return (avg_x, avg_y)
 
-    return (palm_avg_x, palm_avg_y)
+
+def check_if_hand_open(landmarks):
+    # 0 = index, 1 = middle,  2 = ring, 3 = pinky
+    pip = [landmarks[6], landmarks[10],
+            landmarks[14], landmarks[18]]
+    dip = [landmarks[7], landmarks[11],
+            landmarks[15], landmarks[19]]
+
+    for finger in range(4):
+        if pip[finger][1] < dip[finger][1]:
+            return False
+
+    return True
+
+
+def check_left_right(landmarks):
+    # hand 1 = right, hand 0 = left
+    if landmarks[0][0] > landmarks[1][0]:
+        return 1
+    else:
+        return 0
 
 
 def line_distance(x1, x2, y1, y2):
@@ -126,6 +140,7 @@ while camera.isOpened():
     cv2.imshow("original", frame)
 
     img = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
+    image_height, image_width, _ = img.shape
 
     img.flags.writeable = False
     results = hands.process(img)
